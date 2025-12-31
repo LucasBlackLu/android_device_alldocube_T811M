81528b9cc603a6753492769ffb3fce1c3949610b
LucasBlackLu <lucasblack997@gmail.com>
2025-12-31 20:51:57 +0800

    T811M: Import overlay

--- device.mk
+++ device.mk
@@ -204,6 +204,13 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml
 
+# Overlay
+PRODUCT_ENFORCE_RRO_TARGETS := *
+PRODUCT_PACKAGES += \
+    FrameworkRes \
+    FrameworkResExt \
+    WifiRes
+
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
